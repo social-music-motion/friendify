420d07b01505532cc6a8d32064c1f103ea2e4f89
mezzocarattere <annierrosen@gmail.com>
2023-03-29 13:42:14 -0400

    working render tests for login component Co-authored-by: yiningcw <yiningcw@users.noreply.github.com>

--- src/components/__tests__/test.js
+++ src/components/__tests__/test.js
@@ -3,24 +3,61 @@ import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 // import App from '../src/App';
 // import * as axios from 'axios';
 import Login from '../Login.jsx';
+import '@testing-library/jest-dom';
+
 // import config from '../jest.config.js'
 
 // jest.mock('axios'); 
-
+// describe('Unit testing React components', () => {
+  
+// })
 test('login page email label', async () => {
-  // axios.post.mockResolvedValueOnce({ status: 200 });
-  // const { getByLabelText } = render(<Login/>);
-  // const emailInput = getByLabelText('Email')
   // ARRANGE
   render(<Login />)
   // ACT
-  // await userEvent.click(screen.getByText('Load Greeting'))
-  await screen.findByRole('label')
-// sorry just gonna comment this out to see what wasnt working!
-  //expect(screen.getByRole('label', (name): 'Email'), is in document);
+  //  screen.getByLabelText('Email:')
   // ASSERT
-  expect(screen.getByRole('label')).toHaveTextContent('Email:')
-  // expect(screen.getByRole('button')).toBeDisabled()
+  expect(screen.getByLabelText('Email:')).toBeInTheDocument()
 })
 
-//<script type="module" src="your-script.js"></script>
+test('login page password label', async () => {
+  // ARRANGE 
+  render(<Login />)
+  // ACT
+  // const emailLabel = await screen.findByLabelText('Password:')
+  // ASSERT
+  expect(screen.getByLabelText('Password:')).toBeInTheDocument()
+  // expect(emailLabel).toBeInTheDocument();
+}) 
+
+test('check for email text input box', async () => {
+  render(<Login />)
+  const foundInputBox = await screen.findByRole('textbox', { name: /email/i });
+  expect(foundInputBox).toBeInTheDocument();
+})
+
+
+test('check for password text input box', async () => {
+  render(<Login />)
+  expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+})
+
+
+// describe('buttons', () => {
+//   let page;
+//   const loginMock = jest.fn();
+//   let props = {
+//     login: loginMock
+//   }
+
+//   test('the functions passed down to login-button should be invoked on click',  () => {
+//     page = render(<Login {...props} />)
+//     const loginButton =  screen.getByRole('button', {name: 'Login'} )
+//     fireEvent.click(loginButton);
+//     expect(loginMock).toBeCalledTimes(1);
+    
+//   })
+// })
+
+
+
