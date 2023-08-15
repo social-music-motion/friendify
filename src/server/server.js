57bb0d89c2f9b69839fc2f67af1bdc3540b42d17
Ian Flynn <ianflynnwork@gmail.com>
2023-08-15 19:08:13 -0400

    added error handling to mongo connection

--- src/server/server.js
+++ src/server/server.js
@@ -5,65 +5,95 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const accountController = require('./controllers/accountController');
 const cookieController = require('./controllers/cookieController');
-const apiController = require('./controllers/apiController')
-const SpotifyWebApi = require("spotify-web-api-node");
+const apiController = require('./controllers/apiController');
+const SpotifyWebApi = require('spotify-web-api-node');
 const dotenv = require('dotenv').config();
 // Connect to MongoDB
-const MONGO_URI = process.env.MONGO_URI;
-mongoose.connect(MONGO_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: 'spotify-dating-app',
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'spotify-dating-app',
+    });
+    console.log('MongoDB connected!');
+  } catch (error) {
+    console.log('Error connecting to MongoDB: ', error);
   }
-);
+};
+connectDB();
 
 // Set up CORS options to allow passing through cookies to the client server
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: 'GET, POST, PUT, DELETE, OPTIONS',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
 };
-
 app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //TEST
 app.get('/testingroutes', (req, res) => {
-  res.status(200).json('success')
-})
-
-
+  res.status(200).json('success');
+});
 
 // get all matches for particular user
 app.get('/api/getMatches', accountController.getMatches, (req, res) => {
   res.status(200).json(res.locals.allMatches);
 });
 
-
 // Spotify API
-app.get('/api/callback', 
-  apiController.accessAccount,
-  (req, res) => {
-    res.status(200).redirect('http://localhost:3000/signupform');
-  })
-
-app.get('/api/topartists', apiController.getTopTenArtists, apiController.accessRefresh, apiController.getUserData, (req, res) => {
-  res.status(200).json({ topArtists: res.locals.topArtists, userData: res.locals.userData});
-})
-
-app.post('/api/follow', apiController.accessRefresh, apiController.followUser, (req, res) => {
-  res.status(200).json('followed heyianhey') 
-})
-
-  
-// route and handler of sign up
-app.post('/api/signup', accountController.createAccount, accountController.verifyUser, cookieController.setCookies, (req, res) => {
-  res.status(200).json({ created_status: res.locals.account_creation, login: 'did it' });
+app.get('/api/callback', apiController.accessAccount, (req, res) => {
+  res.status(200).redirect('http://localhost:3000/signupform');
 });
+
+app.get(
+  '/api/spotify/getAccessToken',
+  apiController.getAccessToken,
+  (req, res) => {
+    res.status(200).json(res.locals.authorizeURL);
+  }
+);
+
+app.get(
+  '/api/topartists',
+  apiController.getTopTenArtists,
+  apiController.accessRefresh,
+  apiController.getUserData,
+  (req, res) => {
+    res.status(200).json({
+      topArtists: res.locals.topArtists,
+      userData: res.locals.userData,
+    });
+  }
+);
+
+app.post(
+  '/api/follow',
+  apiController.accessRefresh,
+  apiController.followUser,
+  (req, res) => {
+    res.status(200).json('followed heyianhey');
+  }
+);
+
+// route and handler of sign up
+app.post(
+  '/api/signup',
+  accountController.createAccount,
+  accountController.verifyUser,
+  cookieController.setCookies,
+  (req, res) => {
+    res
+      .status(200)
+      .json({ created_status: res.locals.account_creation, login: 'did it' });
+  }
+);
 
 // aroute and handler of logging in
 app.post(
@@ -88,9 +118,9 @@ app.use((err, req, res, next) => {
 });
 
 // Start server on port 8000
-const port = 8000;
-app.listen(port, () => {
-  console.log(`Server has started on ${port}`);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server has started on ${process.env.SERVER_PORT}`);
 });
 
 module.exports = app;
