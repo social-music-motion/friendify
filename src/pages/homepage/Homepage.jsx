52eea6b650f585c0fb5fb93d33bb5a6582a471c7
Ian Flynn <ianflynnwork@gmail.com>
2023-08-15 19:06:12 -0400

    move html string generation to the backend

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -5,34 +5,41 @@ import './style.scss';
 const Homepage = () => {
   const [loggedInToSpotify, setLoggedInToSpotify] = useState(false);
 
-  useEffect(() => {
-    console.log(window.location.href);
-  }, []);
-
-  const requestSpotifyLogin = () => {
+  const requestSpotifyLogin = async () => {
     //SPOTIFY API KEY
-    const client_id = '69f2651f537c4e5681a1b568df57b973';
     //brings back to this page after spotify login
-    const redirect_uri = 'http://localhost:8000/api/callback';
+    // const redirect_uri = 'http://localhost:8000/api/callback';
     //what we want to recieve from spotify
-    var scope = 'user-read-email user-top-read user-follow-modify user-follow-read';
+    //   var scope =
+    //     'user-read-email user-top-read user-follow-modify user-follow-read';
 
-    //localStorage.setItem('client_id', client_id);
-    //localStorage.setItem('client_secret', client_secret);
+    //   //localStorage.setItem('client_id', client_id);
+    //   //localStorage.setItem('client_secret', client_secret);
 
-    let queryString =
-      'https://accounts.spotify.com/en/authorize?' +
-      `client_id=${client_id}` +
-      `&response_type=code` +
-      `&scope=${scope}` +
-      `&redirect_uri=${redirect_uri}` +
-      `&show_dialog=true`;
+    //   let queryString =
+    //     'https://accounts.spotify.com/en/authorize?' +
+    //     `client_id=${client_id}` +
+    //     `&response_type=code` +
+    //     `&scope=${scope}` +
+    //     `&redirect_uri=${redirect_uri}` +
+    //     `&show_dialog=true`;
 
-    window.location.href = queryString;
+    //   // window.location.href = queryString;
+    // };}
+    try {
+      const res = await fetch(
+        'http://localhost:8000/api/spotify/getAccessToken'
+      );
+      const data = await res.json();
+      console.log(data);
+      window.location.href = data;
+    } catch (err) {
+      console.log('error', err);
+    }
   };
 
   return (
-    <div class='centered-page'>
+    <div className='centered-page'>
       <div className='homepage-container feed-stripe'>
         <div id='listen-box'>
           <h1 className='logo'>friendify</h1>
@@ -43,7 +50,9 @@ const Homepage = () => {
               <br />
               Connect Spotify to get started!
             </p>
-            <button id='spot-btn' onClick={requestSpotifyLogin}>Spotify Login</button>
+            <button id='spot-btn' onClick={requestSpotifyLogin}>
+              Spotify Login
+            </button>
           </div>
         </div>
       </div>
